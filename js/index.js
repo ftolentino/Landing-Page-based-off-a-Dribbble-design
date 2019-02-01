fcc4c494380a4c760fdbf5fedac4699b796f9eca
Filmer Tolentino <filmer.tolentino@gmail.com>
2019-01-31 23:18:29 -0800

    created and linked a JavaScript file to the HTML file.
    With js, I wanted to console.log the selected value from the form's <select> elements. I created a function to assign to an onclick event for each <select> element , which console logs its selected value. The next step is to click the button Let's Go Places, once all the forms have options selected. The button can log or alert a message that says "Congratulations! you have "travelerCount" and will be flying from "travelStart" to "travelEnd" starting from "startDate" to "endDate".

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// add an event 'onclick' to console log the selected value from the select list options.
+function getTravelers() {
+    var selectedValue = document.getElementById("travelers").value;
+    console.log(selectedValue);
+}
+
+
+function travelFrom() {
+    var selectedValue = document.getElementById("travel-from").value;
+    console.log(selectedValue);
+}
+
+function travelTo() {
+    var selectedValue = document.getElementById("travel-to").value;
+    console.log(selectedValue);
+}
+
+function startDate() {
+    var selectedValue = document.getElementById("start-date").value;
+    console.log(selectedValue);
+}
+
+function endDate() {
+    var selectedValue = document.getElementById("end-date").value;
+    console.log(selectedValue);
+}
+
+
