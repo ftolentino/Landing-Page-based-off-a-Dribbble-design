65995a625d82bdb4ff13de41a97a62187737ad3b
Filmer Tolentino <filmer.tolentino@gmail.com>
2019-02-12 20:41:46 -0800

    JavaScript, HTML, and CSS changes
    JavaScript: Changed my object from a const to let, so that it's property values could be changed on the fly. I was then able to console.log the new object which logs all the input data changed by the user.
    
    HTML:  I added a new class, .content to the flex parent container of the main content. The new .content class is styled with { position: relative; z-index: 1; } with the purpose of enabling the main content to float/scroll above the background image.
    
    Added a new div with a class .myBackground to migrate the background image styling properties out of the body element . The goal was to not have the background image tile or have a blank white empty space when the screen size reduces width and starts scrolling.
    
    CSS: Started to build for responsive design and established a media query for (max-width: 768px), so that any resolution lower than 768 will start stacking the layout responsively.

--- js/index.js
+++ js/index.js
@@ -3,14 +3,15 @@ function getElementId (element) {
    console.log(element.value); 
 }
 
-const objForm = {
-   id1: travelers.value,  
-   id2: travelFrom.value,
-   id3: travelTo.value,
-   id4: startDate.value,
-   id5: endDate.value,
+let objForm = {
+   travelers: travelers.value,
+   travelFrom: travelFrom.value,
+   travelTo: travelTo.value,
+   startDate: startDate.value,
+   endDate: endDate.value
 };
 
 function submitForm () {
-   console.log(objForm.id1, objForm.id2, objForm.id3, objForm.id4, objForm.id5);
+   console.log(objForm);
 }
+
